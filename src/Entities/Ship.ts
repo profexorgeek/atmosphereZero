2ff433750219577e759e15b79dd161c514ce652e
Justin Johnson <justin.d.johnson@gmail.com>
2021-12-31 10:31:50 -0700

    Ships now navigate to move target

--- src/Entities/Ship.ts
+++ src/Entities/Ship.ts
@@ -11,13 +11,13 @@ import Sprite from 'frostflake/src/Positionables/Sprite';
 
 export default class Ship extends Sprite {
 
-    private readonly shipFrame: Frame           = new Frame(0,16,16,16);
-    private readonly movePrecision: number      = 8;
-    private readonly shipDrag: number           = 1;
-    private readonly speed: number              = 50;
+    private readonly SHIP_FRAME: Frame           = new Frame(0,16,16,16);
+    private readonly MOVE_PRECISION: number      = 8;
+    private readonly DRAG: number                = 1;
+    private readonly MAX_SPEED: number           = 50;
 
     private selectionIndicator: Circle;
-    private _moveTarget: Position               = new Position();
+    private _moveTarget: Position               = null;
     private _selected: boolean                  = false;
     
 
@@ -29,16 +29,17 @@ export default class Ship extends Sprite {
         this.selectionIndicator.visible = this._selected;
     }
 
-
     constructor() {
         super('/content/spritesheet.png');
 
-        this.frame = this.shipFrame;
+        this.frame = this.SHIP_FRAME;
 
-        (<Circle>this.collision).radius = this.shipFrame.width / 2;
+        (<Circle>this.collision).radius = this.SHIP_FRAME.width / 2;
         this.rotation = MathUtil.randomInRange(0, Math.PI * 2);
 
         this.setUpSelectionIndicator();
+
+        this.drag = this.DRAG;
     }
 
     update(): void {
@@ -48,21 +49,31 @@ export default class Ship extends Sprite {
 
         if(this.selected && input.buttonPushed(Mouse.Right))
         {
-            this._moveTarget.x = cursor.worldX;
-            this._moveTarget.y = cursor.worldY;
+            this._moveTarget = new Position(cursor.worldX, cursor.worldY)
         }
 
         this.moveTowardTarget();
     }
 
     private setUpSelectionIndicator(): void {
-        this.selectionIndicator = new Circle(this.shipFrame.width);
+        this.selectionIndicator = new Circle(this.SHIP_FRAME.width);
         this.selectionIndicator.color = Sector.selectionColor;
         this.addChild(this.selectionIndicator);
         this.selected = false;
     }
 
     private moveTowardTarget(): void {
-        this.rotation = this.position.angleTo(this._moveTarget);
+        this.acceleration.reset();
+        if(this._moveTarget !== null)
+        {
+            this.rotation = this.position.angleTo(this._moveTarget);
+            let distToStop = this.velocity.length / this.drag;
+            let distToTarget = this._moveTarget.subtract(this.position).length;
+            if(distToTarget > distToStop + this.MOVE_PRECISION)
+            {
+                this.acceleration.x = Math.cos(this.position.rotation) * this.MAX_SPEED;
+                this.acceleration.y = Math.sin(this.position.rotation) * this.MAX_SPEED;
+            }
+        }
     }
 }

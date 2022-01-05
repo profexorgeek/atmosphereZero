d3cbbb71ad75df03675f042d0e683c37086ecdc0
Justin Johnson <justin.d.johnson@gmail.com>
2022-01-04 20:30:54 -0700

    Lots of new features for Laser entity

--- src/Entities/Laser.ts
+++ src/Entities/Laser.ts
@@ -5,29 +5,39 @@ import Game from "../Game";
 import Data from "frostflake/src/Data/Data";
 import Circle from "frostflake/src/Positionables/Circle";
 import FrostFlake from "frostflake/src/FrostFlake";
+import Ship from "./Ship";
 
 
 export default class Laser extends Sprite {
 
-    private readonly RED_FRAME: Frame        = new Frame(0,8,8,3);
-    private readonly GRN_FRAME: Frame        = new Frame(0,13,3,8);
-    private readonly RADIUS: number          = 6;
-    private readonly SPEED: number           = 200;
-    private readonly RANGE: number           = 200;
+    static readonly RANGE: number           = 150;
+    static readonly RED_FRAME: Frame        = new Frame(0,8,8,3);
+    static readonly GRN_FRAME: Frame        = new Frame(0,13,8,3);
+    static readonly RADIUS: number          = 6;
+    static readonly SPEED: number           = 200;
+    static readonly DAMAGE: number          = 1;
 
     private _lifeSeconds: number = 0;
+    private _owner: Ship                    = null;
+
+    get owner():Ship {
+        return this._owner;
+    }
+
 
     constructor() {
         super(Game.SPRITESHEET);
-        this.frame = this.GRN_FRAME;
-        (<Circle>this.collision).radius = this.RADIUS;
+        this.frame = Laser.GRN_FRAME;
+        (<Circle>this.collision).radius = Laser.RADIUS;
     }
 
-    fire(referencePosition: Position) {
-        this._lifeSeconds = this.RANGE / this.SPEED;
-        this.position = Data.clone(referencePosition);
-        this.velocity.x = Math.cos(this.position.rotation) * this.SPEED;
-        this.velocity.y = Math.sin(this.position.y) * this.SPEED;
+    fire(owner: Ship, direction: number): void {
+        this._lifeSeconds = Laser.RANGE / Laser.SPEED;
+        this._owner = owner;
+        this.position = owner.position.clone();
+        this.rotation = direction;
+        this.velocity.x = Math.cos(this.rotation) * Laser.SPEED;
+        this.velocity.y = Math.sin(this.rotation) * Laser.SPEED;
     }
 
     update(): void {
